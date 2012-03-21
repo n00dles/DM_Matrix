b606ae7de781994e15edb5158316da5f9d950d51
Mike Swan <mike@digimute.com>
2012-03-21 09:30:38 +0000

    fixes tab order

--- DM_Matrix/js/DM_Matrix.js
+++ DM_Matrix/js/DM_Matrix.js
@@ -1,7 +1,7 @@
 
 jQuery(document).ready(function() { 
 
-$('#nav_DM_schema').insertAfter($('#nav_pages'));
+$('#nav_DM_Matrix').insertAfter($('#nav_pages'));
 
 $('#dm_addnew').live("click", function(){
 	$('#DM_addnew_row').stop().slideUp();
