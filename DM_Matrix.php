9b80c241a972c8c3e184b187eeb0242d16046a24
Mike Swan <mike@digimute.com>
2012-04-11 22:49:21 +0100

    editing of tables now returns values to form. need to fix checkboxes and select box

--- DM_Matrix.php
+++ DM_Matrix.php
@@ -42,6 +42,7 @@ if (!is_dir(GSSCHEMAPATH)){
 $defaultDebug = true;
 $schemaArray = array();
 $item_title='Matrix';
+$editing=false; 
 
 require "DM_Matrix/include/sql4array.php";
 $sql = new sql4array();
@@ -105,6 +106,8 @@ if (isset($_GET['add']) && isset($_GET['addrecord'])){
 	$table=$_GET['add'];
 	addRecordFromForm($table);
 	}
+	
+
 
 if (isset($_GET['edit']) && isset($_GET['addfield'])){
   	if (isset($_POST['post-cacheindex'])){
@@ -237,6 +240,8 @@ if (isset($_GET['schema'])) {
 	}
 	elseif (isset($_GET['edit']))
 	{
+
+			
 		$schemaname=$_GET['edit'];
 		echo "<h2>Edit Schema: ".$schemaname."</h2>";
 		?>
@@ -260,11 +265,26 @@ if (isset($_GET['schema'])) {
 		
 		<form method="post" action="load.php?id=DM_Matrix&action=matrix_manager&edit=<?php echo $schemaname; ?>&addfield">
 		<?php if (isset($_GET['field'])){
+			$formName = $_GET['field'];
+			$formType = $schemaArray[$_GET['edit']]['fields'][$_GET['field']];
+			$formDesc= $schemaArray[$_GET['edit']]['desc'][$_GET['field']];
+			$formLabel = $schemaArray[$_GET['edit']]['label'][$_GET['field']];
+			$formHeading = $schemaArray[$_GET['edit']]['desc'][$_GET['field']];
+			$formCacheIndex = $schemaArray[$_GET['edit']]['cacheindex'][$_GET['field']];
+			$formTableView = $schemaArray[$_GET['edit']]['tableview'][$_GET['field']];
+			$editing=true;
 			echo '<h3>Editing Field : '.$_GET['field'].'</h3>'; 
 			$editing=true;
 			
 		} else {
 			echo '<h3>Add New Field</h3>';
+			$formName = "";
+			$formType = "";
+			$formDesc= "";
+			$formLabel = "";
+			$formHeading = "";
+			$formCacheIndex = "";
+			$formTableView = "";
 		}
 		?>
 		<ul class="fields">
@@ -272,7 +292,7 @@ if (isset($_GET['schema'])) {
 				<label class="ui-widget-header fieldstateToggle" for="Inputfield_name">Name</label>
 				<div class="ui-widget-content">
 					<p class="description">Any combination of ASCII letters [a-z], numbers [0-9], or underscores (no dashes or spaces).</p>
-					<input type="text" value="" id="post-name" name="post-name" class="required" size="25">
+					<input type="text" id="post-name" name="post-name" class="required" size="25" <?php echo " value='".$formName."'"; ?> >
 				</div>
 			</li>
 			<li class="InputfieldName Inputfield_name ui-widget" id="wrap_Inputfield_name">
@@ -302,7 +322,7 @@ if (isset($_GET['schema'])) {
 			<label class="ui-widget-header fieldstateToggle" for="Inputfield_name">Add a label</label>
 			<div class="ui-widget-content">
 				<p class="description">Add a label for this Field.</p>
-				<input type="text" value="" id="post-label" name="post-label" class="required" size="115">
+				<input type="text" <?php echo " value='".$formLabel."'"; ?> id="post-label" name="post-label" class="required" size="115">
 				<br/>		
 			</div>
 			</li>
@@ -310,7 +330,7 @@ if (isset($_GET['schema'])) {
 			<label class="ui-widget-header fieldstateToggle" for="Inputfield_name">Add a Description</label>
 			<div class="ui-widget-content">
 				<p class="description">Additional information describing this field and/or instructions on how to enter the content.</p>
-				<input type="text" value="" id="post-desc" name="post-desc" class="required" size="115">
+				<input type="text" <?php echo " value='".$formDesc."'"; ?> id="post-desc" name="post-desc" class="required" size="115">
 				<br/>		
 			</div>
 			</li>
